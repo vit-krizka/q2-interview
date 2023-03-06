49a9209e1a09b31fdefc3c0756b78304b2943238
Vít Křížka <vit.krizka@gmail.com>
2023-03-06 01:46:32 +0100

    přidány tlačíka

--- script/script.js
+++
@@ -1,9 +0,0 @@
-const nxtButton = document.querySelector(".nxt-button");
-const galleryContainer = document.querySelector(".gallery-container");
-
-nxtButton.addEventListener("click", () => {
-    galleryContainer.scrollBy({
-        left: 300,
-        behavior: "smooth",
-    });
-});
